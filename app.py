735498b369715e08f6886ece2e11a3b12a4ccada
Darlan almeida <121138070+Darlan-Almeida@users.noreply.github.com>
2023-05-31 18:09:17 +0000

    inserção no banco de dados

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
 from flask import Flask, render_template, request
+import mysql.connector
+
 
 app = Flask(__name__)
+
+
+# Configurações do banco de dados
+db_config = {
+    'host': 'aws.connect.psdb.cloud',
+    'user': 'jqckc3sb8ggvblsykkhp',
+    'password': 'pscale_pw_Tef8Ao7JbElGKRJF5OHHqEvaVFocf0X1VjLmGmBIC2D',
+    'database': 'vocacional-tech'
+}
+
+# Função para inserir os dados do formulário no banco de dados
+def inserir_dados(nome, email, idade, escola):
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()
+
+    query = "INSERT INTO usuarios (nome, email, idade, escola) VALUES (%s, %s, %s, %s)"
+    values = (nome, email, idade, escola)
+    cursor.execute(query, values)
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
 
 @app.route('/')
 def index():
@@ -13,14 +38,11 @@ def formulario():
     idade = request.form['idade']
     escola = request.form['escola']
 
-    # Faça o que quiser com os dados do formulário
-    # Neste exemplo, estamos apenas imprimindo-os
-    print(f"Nome: {nome}")
-    print(f"E-mail: {email}")
-    print(f"Idade: {idade}")
-    print(f"Escola: {escola}")
-
-    return "Dados do formulário recebidos com sucesso!"
+    try:
+        inserir_dados(nome , email , idade , escola)
+        return "Dados do formulário recebidos com sucesso!"
+    except:
+        return "Os dados não foram enviados"
 
 if __name__ == '__main__':
     app.run()
