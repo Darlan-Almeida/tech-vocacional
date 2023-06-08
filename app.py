d365e3350c650e21581373a6428883f158101a70
Darlan almeida <121138070+Darlan-Almeida@users.noreply.github.com>
2023-06-08 18:52:59 +0000

    respondedo com o resultado, descrição e imagem

--- app.py
+++ app.py
@@ -34,7 +34,6 @@ def submit():
 
     result = realizar_teste(values)
 
-
     print(values , result)
 
     return jsonify(result=result)
@@ -43,7 +42,30 @@ def submit():
 
 @app.route('/resultado/<result>')
 def resultado(result):
-    return render_template('resultado.html', result=result)
+    descricao = ""
+    imagem = ""
+    link= ""
+
+    if (result == "back"):
+        descricao = "Um profissional especializado em desenvolvimento de software e sistemas que trabalha principalmente na parte do servidor, lidando com a lógica de negócios, bancos de dados e integração de sistemas"
+        imagem = "back.svg"
+    elif (result == "front"):
+        descricao = "Um profissional especializado em desenvolvimento de interfaces de usuário, que se concentra na criação da parte visual e interativa de um aplicativo ou site, utilizando tecnologias como HTML, CSS e JavaScript"
+        imagem = "front.svg"
+    elif (result == "full"):
+        descricao = "Um profissional versátil que possui habilidades tanto no desenvolvimento back-end quanto front-end. Eles têm conhecimento em várias tecnologias e podem lidar com todas as camadas de um aplicativo ou site"
+        imagem = "full.svg"
+    elif (result == "dados"):
+        descricao = "Um profissional que trabalha com análise e interpretação de grandes volumes de dados. Eles aplicam técnicas estatísticas e algoritmos para extrair informações valiosas e insights a partir dos dados, ajudando a tomar decisões informadas"
+        imagem = "dados.svg"
+    elif (result == "analista"):
+        descricao = "Um profissional que atua na análise e resolução de problemas relacionados a sistemas de informação. Eles investigam os requisitos do sistema, identificam melhorias e propõem soluções para otimizar a eficiência e a funcionalidade dos sistemas"
+        imagem = "analista.svg"
+    elif (result == "engenheiro"):
+        descricao = "Um profissional que projeta, desenvolve e mantém software de alta qualidade. Eles aplicam princípios de engenharia para construir soluções escaláveis, seguras e eficientes, seguindo as melhores práticas de desenvolvimento de software"
+        imagem = "engenheiro.svg"
+
+    return render_template('resultado.html', result=result , descricao=descricao , imagem=imagem)
 
 if __name__ == '__main__':
     app.run()
