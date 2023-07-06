83b2a5418a8f3a150a2cec1565516350a7106893
Darlan almeida <121138070+Darlan-Almeida@users.noreply.github.com>
2023-07-06 16:18:41 -0300

    Acesso ao BD Atualizado

--- models/config.py
+++ models/config.py
@@ -1,7 +1,7 @@
 DB_CONFIG = {
-    'host': 'chunee.db.elephantsql.com',
-    'database': 'jzujioof',
-    'user': 'jzujioof',
-    'password': 'NSwX4iqTlwIipHEnd-C0JIHKe24_CwJ6'
+    'host': 'host',
+    'database': 'database',
+    'user': 'user',
+    'password': '************'
 }
 
