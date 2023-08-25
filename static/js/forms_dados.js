672a0c09c70808c87ba37e4dc4c22ab10bf0c867
Darlan almeida <121138070+Darlan-Almeida@users.noreply.github.com>
2023-08-25 19:54:40 +0000

    Adicionando funcionalidades de verificação

--- static/js/forms_dados.js
+++ static/js/forms_dados.js
@@ -1,13 +1,23 @@
+var emailInput = document.getElementById("email");
+
+email.onkeyup = function () {
+    if (!validarEmail()) {
+        emailInput.setCustomValidity("O domínio do e-mail não é permitido.");
+    }
+    else {
+        emailInput.setCustomValidity("");
+
+    }
+
+}
+
 function validarEmail(event) {
-    var emailInput = document.getElementById("email");
     var email = emailInput.value;
     var allowedDomains = ["gmail.com", "outlook.com", "yahoo.com", "icloud.com", "hotmail.com", "aol.com", "protonmail.com", "mail.com", "zoho.com", "fastmail.com"];
 
     var emailDomain = email.split('@')[1];
 
     if (allowedDomains.indexOf(emailDomain) === -1) {
-        alert("O domínio do e-mail não é permitido.");
-        event.preventDefault();
         return false; // Impede o envio do formulário
     }
 
